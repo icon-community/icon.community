f31f626a967cf68982028cddd3d10704edce5a44
Brian Li <brian@brianli.com>
2022-03-14 20:46:17 +0900

    Work on colors.

--- themes/icon-community-v1/assets/css/tailwind.config.js
+++ themes/icon-community-v1/assets/css/tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors');
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: ["icon.community_web/themes/icon-community-v1/layouts/**/*.html"],
+  content: ["./themes/icon-community-v1/layouts/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
@@ -32,13 +32,41 @@ module.exports = {
       'icon-lo': '#2ca9b7',
       'icon-dark': '#087083'
     },
-    fontFamily: {
-      montserrat: ['Montserrat', 'sans-serif'],
-      ...defaultTheme.fontFamily
-    },
     screens: {
       'xs': '360px',
       ...defaultTheme.screens,
+    },
+    extend: {
+      backgroundImage: {
+        conic: 'conic-gradient(var(--tw-gradient-stops))',
+        'conic-to-t': 'conic-gradient(at top, var(--tw-gradient-stops))',
+        'conic-to-b': 'conic-gradient(at bottom, var(--tw-gradient-stops))',
+        'conic-to-l': 'conic-gradient(at left, var(--tw-gradient-stops))',
+        'conic-to-r': 'conic-gradient(at right, var(--tw-gradient-stops))',
+        'conic-to-tl': 'conic-gradient(at top left, var(--tw-gradient-stops))',
+        'conic-to-tr': 'conic-gradient(at top right, var(--tw-gradient-stops))',
+        'conic-to-bl':
+          'conic-gradient(at bottom left, var(--tw-gradient-stops))',
+        'conic-to-br':
+          'conic-gradient(at bottom right, var(--tw-gradient-stops))',
+        radial: 'radial-gradient(ellipse at center, var(--tw-gradient-stops))',
+        'radial-at-t':
+          'radial-gradient(ellipse at top, var(--tw-gradient-stops))',
+        'radial-at-b':
+          'radial-gradient(ellipse at bottom, var(--tw-gradient-stops))',
+        'radial-at-l':
+          'radial-gradient(ellipse at left, var(--tw-gradient-stops))',
+        'radial-at-r':
+          'radial-gradient(ellipse at right, var(--tw-gradient-stops))',
+        'radial-at-tl':
+          'radial-gradient(ellipse at top left, var(--tw-gradient-stops))',
+        'radial-at-tr':
+          'radial-gradient(ellipse at top right, var(--tw-gradient-stops))',
+        'radial-at-bl':
+          'radial-gradient(ellipse at bottom left, var(--tw-gradient-stops))',
+        'radial-at-br':
+          'radial-gradient(ellipse at bottom right, var(--tw-gradient-stops))'
+      },
     },
   },
   variants: {
