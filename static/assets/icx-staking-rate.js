80217542aa6f65734c4319fe6f9f1dfd51d46d58
David <98425402+DavidFBD@users.noreply.github.com>
2024-01-04 16:25:25 +0100

    Staking rate fixed (Parrot 9 method applied)

--- static/assets/icx-staking-rate.js
+++ static/assets/icx-staking-rate.js
@@ -1,70 +1,74 @@
-async function getTotalSupply() {
-    var stakingrate = document.getElementById('staking-rate');
-    
-    let myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-  
-    let raw = JSON.stringify({
-        jsonrpc: "2.0",
-        method: "icx_getTotalSupply",
-        id: 1234,
-    });
-  
-    let requestOptions = {
-        method: "POST",
-        headers: myHeaders,
-        body: raw,
-    };
-  
-    const res = await fetch("https://ctz.solidwallet.io/api/v3", requestOptions)
-        const _json = await res.json()
-        const num = Number(_json.result)/1000000000000000000
-        return num
-  }
-  
-  async function getTotalDelegation() {
-      let myHeaders = new Headers();
-      myHeaders.append("Content-Type", "application/json");
-  
-      let raw = JSON.stringify({
-          jsonrpc: "2.0",
-          method: "icx_call",
-          id: 1234,
-          params: {
-              from: "hx0000000000000000000000000000000000000000",
-              to: "cx0000000000000000000000000000000000000000",
-              dataType: "call",
-              data: {
-                  method: "getPReps",
-              }
-          }
-      });
-  
-      let requestOptions = {
-          method: "POST",
-          headers: myHeaders,
-          body: raw,
-      };
-  
-      const res = await fetch("https://api.icon.community/api/v3", requestOptions)
-      const _json = await res.json()
-      const num = Number(_json.result.totalDelegated)/1000000000000000000
-      return num
-    }
-  
-  //   Now I want to divide the total supply by the total delegation to get the staking rate
-  
-  async function getStakingRate() {
-      const totalSupply = await getTotalSupply();
-      const totalDelegation = await getTotalDelegation();
-      const stakingRate = totalSupply * 0.0399 * 0.77 *100 / totalDelegation;  
-      return stakingRate;    
-    }
+const SOLIDWALLET_ENDPOINT = 'https://ctz.solidwallet.io/api/v3';
+const BALANCED_STATS = 'https://balanced.icon.community/api/v1/';
+const TRACKER_ENDPOINT = 'https://tracker.icon.community/api/v1/'
+const LOOP = new BigNumber('1000000000000000000');
+const FETCH_PERIOD = 10000;
 
-  async function setStakingRate() {
-      const stakingRate = await getStakingRate();
-      const stakingRateElement = document.getElementById('staking-rate');
-      stakingRateElement.innerText = stakingRate.toFixed(2);
-  }
+const NETWORK_INFO_PARAMS = {
+    "jsonrpc": "2.0",
+    "id": new Date().getTime(),
+    "method": "icx_call",
+    "params": {
+        "to": "cx0000000000000000000000000000000000000000",
+        "dataType": "call",
+        "data": {
+            "method": "getNetworkInfo"
+        }
+    }
+}
 
-  setStakingRate();
+const IISS_PARAMS = {
+    "jsonrpc": "2.0",
+    "id": new Date().getTime(),
+    "method": "icx_call",
+    "params": {
+        "to": "cx0000000000000000000000000000000000000000",
+        "dataType": "call",
+        "data": {
+            "method": "getIISSInfo"
+        }
+    }
+}
+
+const convertFromLoop = (value) => {
+    return new BigNumber(value).div(LOOP);
+}
+
+const periodicallyFetchData = () => {
+    fetch(`${BALANCED_STATS}tokens?symbol=ICX`)
+        .then(res => res.json()
+            .then(data => {
+                const price = new BigNumber(data[0].price);
+                const supply = new BigNumber(data[0].total_supply);
+                const avgFeeOffset = new BigNumber(0.1);
+
+                document.getElementById('async-price').innerHTML = `$${price.toFormat(3)}`;
+                document.getElementById('async-marketcap').innerHTML = `$${price.times(supply).toFormat(0)}`;
+                document.getElementById('async-fee').innerHTML = `~ $${price.times(avgFeeOffset).toFormat(5)}`;
+                document.getElementById('async-price-transfer').innerHTML = `$${price.times(0.00125).toFormat(4)}`;
+                document.getElementById('async-price-asset').innerHTML = `$${price.times(0.007).toFormat(4)}`;
+                document.getElementById('async-price-contract').innerHTML = `$${price.times(0.2).toFormat(4)}`;
+            })
+        );
+
+    fetch(SOLIDWALLET_ENDPOINT, { method: "POST", body: JSON.stringify(NETWORK_INFO_PARAMS) })
+        .then(res => res.json()
+            .then(networkInfoData => {
+                fetch(SOLIDWALLET_ENDPOINT, { method: "POST", body: JSON.stringify(IISS_PARAMS) })
+                    .then(res => res.json()
+                        .then(IISSData => {
+                            const totalDelegated = new BigNumber(networkInfoData.result.totalDelegated);
+                            const totalBonded = new BigNumber(networkInfoData.result.totalBonded);
+                            const stakingApr = new BigNumber(IISSData.result.variable.Iglobal)
+                                .times(new BigNumber(IISSData.result.variable.Ivoter).times(12))
+                                .div(totalDelegated.plus(totalBonded))
+                            document.getElementById('staking-rate').innerHTML = `${stakingApr.toFormat(2)}%`;
+                        })
+                    )
+            })
+        )
+}
+
+
+setInterval(() => periodicallyFetchData(), FETCH_PERIOD)
+periodicallyFetchData();
