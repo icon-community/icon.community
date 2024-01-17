e2de4ab1e2b29ec5de94d89f3d901d419f291709
David <98425402+DavidFBD@users.noreply.github.com>
2024-01-17 16:10:22 +0100

    Youtube and Medium

---
+++ static/assets/fetchMediumRssFeed.js
@@ -0,0 +1,55 @@
+function formatDate(dateString) {
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const date = new Date(dateString);
+    return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+}
+
+function extractImageUrl(htmlContent) {
+    const parser = new DOMParser();
+    const htmlDoc = parser.parseFromString(htmlContent, "text/html");
+    const imgElement = htmlDoc.querySelector("img");
+    return imgElement ? imgElement.src : null;
+}
+
+async function fetchMediumRssFeed() {
+    const proxyUrl = 'https://api.allorigins.win/raw?url=';
+    const mediumRssFeedUrl = 'https://medium.com/feed/helloiconworld';
+    const url = proxyUrl + encodeURIComponent(mediumRssFeedUrl);
+
+    try {
+        const response = await fetch(url);
+        const data = await response.text();
+
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(data, "text/xml");
+
+        // Process the XML data here
+        const items = xmlDoc.getElementsByTagName("item");
+        const latestItem = items[0]; // Fetch the latest item
+
+        // Extract information
+        let title = latestItem.getElementsByTagName("title")[0].textContent;
+        let link = latestItem.getElementsByTagName("link")[0].textContent;
+        let pubDate = latestItem.getElementsByTagName("pubDate")[0].textContent;
+        let contentEncoded = latestItem.getElementsByTagName("content:encoded")[0].textContent;
+        
+        // Extract the image URL from content:encoded
+        let imageUrl = extractImageUrl(contentEncoded);
+        console.log(imageUrl)
+        // Format the publication date
+        let formattedDate = formatDate(pubDate);
+
+        // Update the HTML elements
+        document.getElementById('medium-title').textContent = title;
+        document.getElementById('medium-date').textContent = formattedDate;
+        document.getElementById('medium-cta').setAttribute('href', link);
+        
+        if (imageUrl) {
+            document.getElementById('medium-image').src = imageUrl;
+        }
+    } catch (error) {
+        console.error('Error fetching Medium RSS feed:', error);
+    }
+}
+
+fetchMediumRssFeed();
---
+++ static/assets/fetchYoutubeRssFeed.js
@@ -0,0 +1,42 @@
+async function fetchYoutubeRssFeed() {
+    const proxyUrl = 'https://api.allorigins.win/raw?url=';
+    const youtubeRssFeedUrl = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCI7Z_1sTKN-kCVgFD2a0GXQ';
+    const url = proxyUrl + encodeURIComponent(youtubeRssFeedUrl);
+
+    try {
+        const response = await fetch(url);
+        const data = await response.text();
+
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(data, "text/xml");
+
+        // Process the XML data here
+        const entries = xmlDoc.getElementsByTagName("entry");
+        const latestEntry = entries[0]; // Fetch the latest entry
+
+        // Extract information
+        let title = latestEntry.getElementsByTagName("title")[0].textContent;
+        let link = latestEntry.getElementsByTagName("link")[0].getAttribute("href");
+        let pubDate = latestEntry.getElementsByTagName("published")[0].textContent;
+        let thumbnailUrl = latestEntry.getElementsByTagName("media:thumbnail")[0].getAttribute("url");
+
+        // Format the publication date
+        let formattedDate = formatDate(pubDate);
+
+        // Update the HTML elements
+        document.getElementById('youtube-title').textContent = title;
+        document.getElementById('youtube-date').textContent = formattedDate;
+        document.getElementById('youtube-image').src = thumbnailUrl;
+        document.getElementById('youtube-cta').onclick = () => window.open(link, '_blank');
+        
+    } catch (error) {
+        console.error('Error fetching YouTube RSS feed:', error);
+    }
+}
+
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    return date.toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' });
+}
+
+fetchYoutubeRssFeed();
